4a8e2fbaf3b0d7fdd346061fe19ff76154cc5599
chriscauley <chris@lablackey.com>
2019-08-01 15:11:12 +0000

    init

---
+++ test/sample.spec.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe('sample test', function () {
+  it('should work', async function () {
+    console.log(await browser.version());
+
+    expect(true).to.be.true;
+  });
+});
